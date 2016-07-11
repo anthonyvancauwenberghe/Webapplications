bf1458b443d0d132bada30d362c7858b5a0a3004
tony <anthony.vanc@gmail.com>
2016-07-11 23:46:09 +0200

    testing

--- Dashboard/login.php
+++ Dashboard/login.php
@@ -57,7 +57,7 @@ if ($login->login_check()) {
 <body style="background:#F7F7F7;">
 <center><a href="#">
         <div class="logo">
-            <img style="width:44%" class="loginlogo" alt="Logo" src="images/logo.png">
+            <img style="width:44%" class="loginlogo" alt="ArteroPK Logo" src="images/logo.png">
         </div>
     </a></center>
 <div class="">
@@ -67,13 +67,13 @@ if ($login->login_check()) {
     <div id="wrapper">
         <div id="login" class=" form">
             <section class="login_content">
-                <form action="" method="GET">
+                <form action="" method="get">
                     <h1>Login Form</h1>
                     <div>
-                        <input name="username" type="text" class="form-control" placeholder="Username" required=""/>
+                        <input name="username" type="text" class="form-control" placeholder="Username" required="" />
                     </div>
                     <div>
-                        <input name="p" type="password" class="form-control" placeholder="Password" required=""/>
+                        <input name="p" type="password" class="form-control" placeholder="Password" required="" />
                     </div>
                     <div>
 
@@ -82,11 +82,45 @@ if ($login->login_check()) {
                     <div class="clearfix"></div>
 
                     <div class="clearfix"></div>
-                    <br/>
+                    <br />
                     <div class="separator">
                         <div>
 
                             <p>©2016 All Rights Reserved DeviousPs </p>
+                        </div>
+                    </div>
+                </form>
+            </section>
+        </div>
+
+        <div id="register" class=" form">
+            <section class="login_content">
+                <form>
+                    <h1>Create Account</h1>
+                    <div>
+                        <input type="text" class="form-control" placeholder="Username" required="" />
+                    </div>
+                    <div>
+                        <input type="email" class="form-control" placeholder="Email" required="" />
+                    </div>
+                    <div>
+                        <input type="password" class="form-control" placeholder="Password" required="" />
+                    </div>
+                    <div>
+                        <a class="btn btn-default submit" href="index.html">Submit</a>
+                    </div>
+                    <div class="clearfix"></div>
+                    <div class="separator">
+
+                        <p class="change_link">Already a member ?
+                            <a href="#tologin" class="to_register"> Log in </a>
+                        </p>
+                        <div class="clearfix"></div>
+                        <br />
+                        <div>
+                            <h1><i class="fa fa-paw" style="font-size: 26px;"></i> Gentelella Alela!</h1>
+
+                            <p>©2015 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
                         </div>
                     </div>
                 </form>
