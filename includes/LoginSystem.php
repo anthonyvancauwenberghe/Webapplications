c65b519e0c5e1529129ff1ee1b72547c5462069e
tony <anthony.vanc@gmail.com>
2016-07-11 23:14:03 +0200

    test

--- includes/LoginSystem.php
+++ includes/LoginSystem.php
@@ -201,7 +201,10 @@ class LoginSystem
                 exit();
             } else {
                 // Login failed
-                header('Location: ../login.php?error=1');
+                echo $username;
+                echo '<br>';
+                echo $password;
+                //header('Location: ../login.php?error=');
                 exit();
             }
         } else {
