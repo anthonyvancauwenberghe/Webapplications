efcfce1f043a605a46da8f4de2bc230e37c53646
tony <anthony.vanc@gmail.com>
2016-07-13 00:09:44 +0200

    fixed

--- includes/Logs.php
+++ includes/Logs.php
@@ -16,27 +16,21 @@ class Logs
         echo '<thead>
                         <tr>
                           <th>Playername</th>
-                          <th>GP Value</th>
-                          <th>DP Value</th>
+                          <th>GP Value (Mil)</th>
+                          <th>DP Value ($)</th>
                         </tr>
                       </thead>
                       
                       <tbody>';
         foreach ($playerValuesArray as $key => $playerValue) {
+
             if(isset($playerValue)){
                 echo '<tr>';
                      echo '<td>' . $playerValue["name"] . '</td>';
                      echo '<td>' . $playerValue["gp"] . '</td>';
                      echo '<td>' . $playerValue["dp"] . '</td>';
-                echo '<tr>';
+                echo '</tr>';
             }
-            /*
-            echo '<tr>
-                          <td>Donna</td>
-                          <td>Snider</td>
-                          <td>Customer Support</td>
-                        </tr>';
-            */
         }
         echo '</tbody>';
 
