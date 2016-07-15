94f8620c12f8b8f501fa0e748f60903d14844a49
tony <anthony.vanc@gmail.com>
2016-07-15 04:53:50 +0200

    fix

--- includes/Data/LogsData.php
+++ includes/Data/LogsData.php
@@ -7,7 +7,7 @@ class LogsData extends Data
     {
         //$query = array(array('$and' =>array('log-type' => 'trade-log'), array('$or' => array(array('content.user.player-name' => $name), array('content.user-2.player-name' => $name)))));
 
-        $query = array('$and' => array('log-type' => 'trade-log'), array('$or' => array(array('content.user.player-name' => $name), array('content.user-2.player-name' => $name))));
+        $query = array('$and' => array(array('log-type' => 'trade-log'), array('$or' => array(array('content.user.player-name' => $name), array('content.user-2.player-name' => $name)))));
 
         $cursor = $this->find(Collection::LOGS, $query);
         return $cursor;
