73a04b55f9a9ff00824d6e0a488c27e2db7b700f
tony <anthony.vanc@gmail.com>
2016-07-30 15:04:22 +0200

    lets test it

--- includes/Data/CameraData.php
+++ includes/Data/CameraData.php
@@ -16,7 +16,8 @@ class CameraData extends Data
     }
 
     private function buildDocument(){
-        $xml = $this->getXML();
+        $xmlstr = $this->getXML();
+        $xml = new SimpleXMLElement($xmlstr);
         $timestamp = $xml->Timestamp;
         $source = $xml->Source->Name;
         $plate = $xml->Object[0]->Value;
@@ -24,10 +25,10 @@ class CameraData extends Data
         $height = $xml->Snapshot[0]->Height;
         $width = $xml->Snapshot[0]->Width;
         $image = $xml->Snapshot[0]->Image;
-        $document = array('timestamp' => $timestamp,
+        $document = array('timestamp' => $timestamp, 'data'=> $xmlstr,
             'content' => array('plate' => $plate, 'confidence' => $confidence, 'source' => $source,
                 'image' => array('height' => $height, 'width' => $width, 'image' => $image)));
-        
+
         return $document;
     }
 
