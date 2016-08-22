9697b2bcaef04452bfe335b2e0852cfc3664a72c
tony <anthony.vanc@gmail.com>
2016-08-22 22:16:41 +0200

    autoloader fix

--- libs/AutoLoader.php
+++ libs/AutoLoader.php
@@ -65,7 +65,7 @@ function autoloader($class_paths = NULL, $use_base_dir = true)
 
 
     static $conf = [
-        'basepath' => 'D:\xampp\htdocs\Webapplications',
+        'basepath' => '/var/www/html/',
         'debug' => false,
         'extensions' => ['.php'], // multiple extensions ex: ['.php', '.class.php']
         'namespace' => '',
