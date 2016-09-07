a764b91c1a73d1d14309f3f6f40b50295ce16bea
tony <anthony.vanc@gmail.com>
2016-09-07 23:09:27 +0200

    img fix

--- includes/Display/Template.php
+++ includes/Display/Template.php
@@ -446,7 +446,7 @@ class Template
                     <ul class="nav navbar-nav navbar-right">
                         <li class="">
                             <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                                <img src="images/img.png" alt="">' . ucfirst($this->login->getName()) . '
+                                <img src="../images/img.png" alt="">' . ucfirst($this->login->getName()) . '
 <span class=" fa fa-angle-down"></span>
 </a>
 <ul class="dropdown-menu dropdown-usermenu pull-right">
